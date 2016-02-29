f36e39980e90fd2d8b5d321d4e1f46823a10ddce
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-29 13:36:26 +0100

    New dist files

--- styleguide.dev/dist/js/hbg-prime.dev.js
+++ styleguide.dev/dist/js/hbg-prime.dev.js
@@ -9832,6 +9832,8 @@ return jQuery;
 
 var HelsingborgPrime = {};
 
+$('html, body').removeClass('no-js');
+
 //
 // @name Language
 //
@@ -10504,6 +10506,35 @@ HelsingborgPrime.Prompt.ModalLimit = (function ($) {
     };
 
     return new ModalLimit();
+
+})(jQuery);
+
+//
+// @name Search top
+// @description  Open the top search
+//
+HelsingborgPrime = HelsingborgPrime || {};
+HelsingborgPrime.Prompt = HelsingborgPrime.Prompt || {};
+
+HelsingborgPrime.Prompt.SearchTop = (function ($) {
+
+    function SearchTop() {
+        this.bindEvents();
+    }
+
+    SearchTop.prototype.bindEvents = function () {
+        $('.toggle-search-top').on('click', function (e) {
+            this.toggle(e);
+        }.bind(this));
+    };
+
+    SearchTop.prototype.toggle = function (e) {
+        e.preventDefault();
+        $('.search-top').slideToggle(300);
+        $('.search-top').find('input[type=search]').focus();
+    };
+
+    return new SearchTop();
 
 })(jQuery);
 
