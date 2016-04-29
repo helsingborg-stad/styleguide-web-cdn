ac1a7fd7cf90e6cf1e1943043e03325d6a1313d4
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-29 09:26:35 +0200

    Updated dist

--- styleguide.dev/dist/js/hbg-prime.dev.js
+++ styleguide.dev/dist/js/hbg-prime.dev.js
@@ -12684,15 +12684,30 @@ HelsingborgPrime.Helper.EqualHeight = (function ($) {
     function EqualHeight() {
         $(function(){
 
-            this.init();
-
-            $(window).on('resize', function () {
-                this.destroy();
+            // Initialize if flexbox not supported
+            if (!this.supportsFlexbox()) {
                 this.init();
-            }.bind(this));
+
+                $(window).on('resize', function () {
+                    this.destroy();
+                    this.init();
+                }.bind(this));
+            }
 
         }.bind(this));
     }
+
+    /**
+     * Check if browser supports flexbox
+     * @return {boolean}
+     */
+    EqualHeight.prototype.supportsFlexbox = function () {
+        if (typeof document.createElement("p").style.flexWrap != 'undefined' && document.createElement("p").style.flexWrap == '') {
+            return true;
+        }
+
+        return false;
+    };
 
     /**
      * Resets heights to auto
@@ -12788,6 +12803,87 @@ HelsingborgPrime.Helper.Menu = (function ($) {
     };
 
     return new Menu();
+
+})(jQuery);
+
+//
+// @name Menu priority
+//
+HelsingborgPrime = HelsingborgPrime || {};
+HelsingborgPrime.Helper = HelsingborgPrime.Helper || {};
+
+HelsingborgPrime.Helper.MenuPriority = (function ($) {
+
+    var $nav = null;
+    var $btn = null;
+    var $vlinks = null;
+    var $hlinks = null;
+
+    var availableSpace = 0;
+    var breaks = [];
+
+    function MenuPriority() {
+        if ($('.header-jumbo').length > 0 && !$('#main-menu').hasClass('nav-justify')) {
+            this.init();
+        }
+    }
+
+    MenuPriority.prototype.init = function () {
+        $nav = $('#main-menu').parent('.nav-group-overflow');
+        $vlinks = $('#main-menu');
+        $hlinks = $nav.find('.nav-grouped-overflow');
+        $btn = $nav.find('.dropdown-toggle');
+
+        this.updateNavigation();
+
+        $(window).on('resize', function (e) {
+            this.updateNavigation();
+        }.bind(this));
+    };
+
+    MenuPriority.prototype.updateNavigation = function () {
+        availableSpace = $btn.is(':visible') ? $nav.parent().first().width() - ($btn.width() + parseFloat($nav.attr('data-btn-width'))) : $nav.parent().first().width();
+
+        // The visible list is overflowing the available space
+        if ($vlinks.width() > 0 && $vlinks.width() > availableSpace) {
+
+            // Record vlinks width
+            breaks.push($vlinks.width());
+
+            // Move last element to the grouped items
+            $vlinks.children().last().prependTo($hlinks);
+            $hlinks.removeClass('hidden');
+            $btn.removeClass('hidden').attr('data-item-count', breaks.length);
+        } else {
+
+            // Check if there's space to move an item back to the nav
+            if (availableSpace > breaks[breaks.length-1]) {
+                $hlinks.children().first().appendTo($vlinks);
+                breaks.pop();
+                $btn.attr('data-item-count', breaks.length);
+            }
+
+            if (breaks.length < 1) {
+                $hlinks.addClass('hidden');
+                $btn.addClass('hidden').attr('data-item-count', breaks.length);
+            }
+        }
+
+        /*
+        console.table({
+            'visible-links' : $vlinks.width(),
+            'available-space' : availableSpace,
+            'breaks' : breaks.length
+        });
+        */
+
+        // Rerun if nav is still overflowing
+        if ($nav.is(':visible') && $vlinks.width() > availableSpace && breaks.length > 0 && breaks.length < $vlinks.children('li').length) {
+            this.updateNavigation();
+        }
+    };
+
+    return new MenuPriority();
 
 })(jQuery);
 
