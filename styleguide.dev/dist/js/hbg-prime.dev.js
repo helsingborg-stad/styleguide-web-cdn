317512742c135543e0a81cbb72e0362255e6d834
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-17 13:01:18 +0200

    Updated

--- styleguide.dev/dist/js/hbg-prime.dev.js
+++ styleguide.dev/dist/js/hbg-prime.dev.js
@@ -12295,6 +12295,31 @@ HelsingborgPrime.Component.Accordion = (function ($) {
 })(jQuery);
 
 //
+// @name Modal
+// @description  Show accodrion dropdown, make linkable by updating adress bar
+//
+HelsingborgPrime = HelsingborgPrime || {};
+HelsingborgPrime.Component = HelsingborgPrime.Component || {};
+
+HelsingborgPrime.Component.Dropdown = (function ($) {
+
+    function Dropdown() {
+        this.handleEvents();
+    }
+
+    Dropdown.prototype.handleEvents = function () {
+        $('[data-dropdown]').on('click', function (e) {
+            var targetElement = $(this).attr('data-dropdown');
+            $(this).parent().find(targetElement).toggle();
+            $(this).parent().find(targetElement).find('input[data-dropdown-focus]').focus();
+        });
+    };
+
+    return new Dropdown();
+
+})(jQuery);
+
+//
 // @name Gallery
 // @description  Popup boxes for gallery items.
 //
