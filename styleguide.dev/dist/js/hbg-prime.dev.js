33a5aa16b8d83652c84605169b582621eee4d93b
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-07-01 15:04:56 +0200

    Updated

--- styleguide.dev/dist/js/hbg-prime.dev.js
+++ styleguide.dev/dist/js/hbg-prime.dev.js
@@ -12253,7 +12253,7 @@ HelsingborgPrime.Component.Accordion = (function ($) {
     }
 
     Accordion.prototype.init = function () {
-        $('.accordion-toggle').on('click', function(event) {
+        $('label.accordion-toggle').on('click', function(event) {
 			window.location.hash = '#' + $(this).attr('for');
 		});
 
@@ -12735,6 +12735,159 @@ HelsingborgPrime.Component.Slider = (function ($) {
     };
 
     return new Slider();
+
+})(jQuery);
+
+//
+// @name Modal
+// @description  Show accodrion dropdown, make linkable by updating adress bar
+//
+HelsingborgPrime = HelsingborgPrime || {};
+HelsingborgPrime.Component = HelsingborgPrime.Component || {};
+HelsingborgPrime.Component.TagManager = (function ($) {
+
+    var typingTimer;
+
+    function TagManager() {
+        $('.tag-manager').each(function (index, element) {
+            this.init(element);
+        }.bind(this));
+
+        $(document).on('click', '.tag-manager .tag-manager-selected button[data-action="remove"]', function (e) {
+            e.preventDefault();
+
+            var tagElement = $(e.target).closest('li');
+            this.removeTag(tagElement);
+        }.bind(this));
+    }
+
+    /**
+     * Initialize tag manager
+     * @param  {element} element The tag manager element
+     * @return {void}
+     */
+    TagManager.prototype.init = function(element) {
+        var $element = $(element);
+        var $button = $element.find('.tag-manager-input [name="add-tag"]');
+        var $input = $element.find('.tag-manager-input input[type="text"]');
+
+        $button.on('click', function (e) {
+            e.preventDefault();
+            var tag = $input.val();
+
+            this.addTag(element, tag);
+        }.bind(this));
+
+        $input.on('keypress', function (e) {
+            if (e.keyCode !== 13) {
+                return;
+            }
+
+            e.preventDefault();
+            this.addTag($(e.target).parents('.tag-manager')[0], $input.val());
+        }.bind(this));
+
+        if ($element.attr('data-wp-ajax-action') && typeof ajaxurl !== 'undefined') {
+            $input.on('keyup', function (e) {
+                clearTimeout(typingTimer);
+
+                typingTimer = setTimeout(function () {
+                    this.autocompleteQuery(element);
+                }.bind(this), 300);
+            }.bind(this));
+
+            $('.tag-manager').on('click', '.autocomplete button', function (e) {
+                e.preventDefault();
+                var tag = $(e.target).closest('button').val();
+                var element = $(e.target).closest('button').parents('.tag-manager');
+
+                this.addTag(element, tag);
+            }.bind(this));
+        }
+    };
+
+    /**
+     * Do ajax autocomplete request
+     * @param  {element} element The tag manager element
+     * @return {void}
+     */
+    TagManager.prototype.autocompleteQuery = function(element) {
+        var $element = $(element);
+        var $input = $element.find('.tag-manager-input input[type="text"]');
+
+        // Return if no search value
+        if ($input.val().length === 0) {
+            clearTimeout(typingTimer);
+            $element.find('.autocomplete').remove();
+            return false;
+        }
+
+        var ajaxAction = $element.attr('data-wp-ajax-action');
+        var data = {
+            action: ajaxAction,
+            q: $input.val()
+        };
+
+        $.post(ajaxurl, data, function (res) {
+            if (res.length === 0) {
+                return;
+            }
+
+            this.showAutocomplete(element, res);
+        }.bind(this), 'JSON');
+    };
+
+    /**
+     * Show the autocomplete element
+     * @param  {element} element The tag manager eleement
+     * @param  {array} items     The autocomplete items
+     * @return {void}
+     */
+    TagManager.prototype.showAutocomplete = function(element, items) {
+        var $element = $(element);
+        $element.find('.autocomplete').remove();
+
+        var $autocomplete = $('<div class="autocomplete"><ul></ul></div>');
+
+        $.each(items, function (index, item) {
+            $autocomplete.find('ul').append('<li><button value="' + item + '">' + item + '</button></li>');
+        });
+
+        $element.find('.tag-manager-input').append($autocomplete);
+    };
+
+    /**
+     * Adds a tag to the tag manager selected tags
+     * @param {element} element The tag manager element
+     * @param {string} tag      The tag name
+     */
+    TagManager.prototype.addTag = function(element, tag) {
+        if (tag.length === 0) {
+            return;
+        }
+
+        var $element = $(element);
+        var inputname = $(element).attr('data-input-name');
+        $element.find('.tag-manager-selected ul').append('<li class="label">\
+            <button class="btn btn-plain" data-action="remove">&times;</button>\
+            ' + tag + '\
+            <input type="hidden" name="' + inputname + '[]" value="' + tag + '">\
+        </li>');
+
+        $element.find('.tag-manager-input input[type="text"]').val('');
+        $element.find('.autocomplete').remove();
+    };
+
+    /**
+     * Removes a selected tag
+     * @param  {element} tagElement The tag to remove
+     * @return {void}
+     */
+    TagManager.prototype.removeTag = function(tagElement) {
+        $(tagElement).remove();
+    };
+
+    return new TagManager();
 
 })(jQuery);
 
